e6ed13e7fa0354875cfde0362aafe03e1169b2d3
Krisha <kravula@unc.edu>
2025-01-27 02:46:56 +0000

    followed tutorial

---
+++ your-project-name/target/debug/deps/your_project_name-205fd8fcd787565a.d
@@ -0,0 +1,5 @@
+/workspaces/rust-project/your-project-name/target/debug/deps/libyour_project_name-205fd8fcd787565a.rmeta: src/main.rs
+
+/workspaces/rust-project/your-project-name/target/debug/deps/your_project_name-205fd8fcd787565a.d: src/main.rs
+
+src/main.rs:
---
+++ your-project-name/target/debug/deps/your_project_name-f2d71a912643e384.d
@@ -0,0 +1,5 @@
+/workspaces/rust-project/your-project-name/target/debug/deps/libyour_project_name-f2d71a912643e384.rmeta: src/main.rs
+
+/workspaces/rust-project/your-project-name/target/debug/deps/your_project_name-f2d71a912643e384.d: src/main.rs
+
+src/main.rs:
